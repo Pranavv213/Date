a18da59d48326addaea21c7ce056a5773d74ebc1
Pranav <pranavvermaofiicial@gmail.com>
2022-08-14 16:54:21 +0530

    3

--- src/firebase.js
+++ src/firebase.js
@@ -4,16 +4,16 @@ import 'firebase/compat/firestore'
 import 'firebase/compat/storage'
 import { initializeApp } from "firebase/app";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBR0M9Ksbrp2JdAQ_Kpa2ZsZQQ4a8Z9cUY",
-  authDomain: "restaurant-7b1f1.firebaseapp.com",
-  projectId: "restaurant-7b1f1",
-  storageBucket: "restaurant-7b1f1.appspot.com",
-  messagingSenderId: "902056945484",
-  appId: "1:902056945484:web:5c648a069571f498bf91e9",
+  apiKey: "AIzaSyA7QVBQNM6TjhvvBiFhENyIeiccJY4nlmI",
+  authDomain: "date-a4c64.firebaseapp.com",
+  projectId: "date-a4c64",
+  storageBucket: "date-a4c64.appspot.com",
+  messagingSenderId: "832868718381",
+  appId: "1:832868718381:web:d1fb41a9bf19e8aa3825a4",
   
 };
-
 
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
